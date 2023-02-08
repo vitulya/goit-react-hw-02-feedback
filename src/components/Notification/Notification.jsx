d32898812fcac404ef72bc054f0f7fb68c736fdd
Vitulya <vitulya.bondarchuk@gmail.com>
2023-02-08 13:20:49 +0200

    propType

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,5 +1,10 @@
-export const Notification = ({message}) =>{
-    return(
-        <span>{message}</span>
-    )
-}
+import PropTypes from 'prop-types';
+
+export const Notification = ({ message }) => {
+  return <span>{message}</span>;
+};
+
+Notification.propTypes = {
+    message: PropTypes.string.isRequired,
+};
+
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,4 +1,5 @@
 import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
 
 export const Statistics = ({
   good,
@@ -16,4 +17,12 @@ export const Statistics = ({
       <li>PositivePercentage: {positivePercentage} %</li>
     </ul>
   );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
